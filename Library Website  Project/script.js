00f14acc6b01f6a19de904770be81583c7789a03
Xdpreston2 <xavierpreston2@gmail.com>
2024-11-25 12:41:05 -0500

    Delete Library Website  Project directory

--- Library Website  Project/script.js
+++
@@ -1,7 +0,0 @@
-let cart = [];
-
-function addToCart(bookTitle) {
-    cart.push(bookTitle);
-    alert(`${bookTitle} has been added to your cart.`);
-    console.log('Cart:', cart);  // To check the cart in the console
-}
